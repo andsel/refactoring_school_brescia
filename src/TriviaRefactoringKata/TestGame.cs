f0b29782dee08191bfd37d341ffc45315e24cb06
IEUser <IEUser>
2016-11-24 01:32:51 -0800

    Added xUnit and created golden master

---
+++ src/TriviaRefactoringKata/TestGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Trivia;
+using Xunit;
+
+namespace UglyTrivia
+{
+    public class GoldenMasterTests
+    {
+        [Fact]
+        public void MainTest()
+        {
+            var stdOut = new StringWriter();
+            Console.SetOut(stdOut);
+            GameRunner.Main(null);
+
+            Assert.Equal("", stdOut.ToString());
+        }
+    }
+}
