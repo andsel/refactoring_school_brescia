2915d21217961c381fc2db7251922a89139552e1
IEUser <IEUser>
2016-11-24 03:30:18 -0800

    Added golden master

--- src/TriviaRefactoringKata/TestGame.cs
+++ src/TriviaRefactoringKata/TestGame.cs
@@ -8,13 +8,32 @@ namespace UglyTrivia
     public class GoldenMasterTests
     {
         [Fact]
-        public void MainTest()
+        public void TriviaGameTest()
         {
-            var stdOut = File.CreateText("output.txt");
-            Console.SetOut(stdOut);
-            GameRunner.Run(new Random(10));
+            RunGame();
+            //var actual = ReadLastOutput();
+            //var expected = ReadGoldenMaster();
+
+            //Assert.Equal(expected, actual);
+
+            //var stdOut = File.CreateText("output.txt");
+            //Console.SetOut(stdOut);
+            //GameRunner.Run(new Random(10));
 
             //Assert.Equal("", stdOut.ToString());
+        }
+
+        private void RunGame()
+        {
+            using (var stdout = File.CreateText("actual.txt"))
+            {
+                Console.SetOut(stdout);
+                for (int i = 0; i < 1000; i++)
+                {
+                    var seed = 765456 + 42 * i;
+                    GameRunner.Run(new Random(seed));
+                }
+            }
         }
     }
 }
