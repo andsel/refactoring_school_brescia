2b56458ac16b5b903a2af2f5ba28f1d22b42affb
IEUser <IEUser>
2016-11-24 03:40:43 -0800

    Golden master finish

--- src/TriviaRefactoringKata/TestGame.cs
+++ src/TriviaRefactoringKata/TestGame.cs
@@ -7,25 +7,33 @@ namespace UglyTrivia
 {
     public class GoldenMasterTests
     {
+        private static readonly string ACTUAL_FILENAME = "actual.txt";
+        private static readonly string GOLDEN_MASTER = "../../golden_master.txt";
+
         [Fact]
         public void TriviaGameTest()
         {
             RunGame();
-            //var actual = ReadLastOutput();
-            //var expected = ReadGoldenMaster();
+            var actual = ReadLastOutput();
+            var expected = ReadGoldenMaster();
 
-            //Assert.Equal(expected, actual);
+            Assert.Equal(expected, actual);
+        }
 
-            //var stdOut = File.CreateText("output.txt");
-            //Console.SetOut(stdOut);
-            //GameRunner.Run(new Random(10));
+        private String ReadLastOutput()
+        {
+            return File.ReadAllText(ACTUAL_FILENAME);
+        }
 
-            //Assert.Equal("", stdOut.ToString());
+        private String ReadGoldenMaster()
+        {
+            
+            return File.ReadAllText(GOLDEN_MASTER);
         }
 
         private void RunGame()
         {
-            using (var stdout = File.CreateText("actual.txt"))
+            using (var stdout = File.CreateText(ACTUAL_FILENAME))
             {
                 Console.SetOut(stdout);
                 for (int i = 0; i < 1000; i++)
