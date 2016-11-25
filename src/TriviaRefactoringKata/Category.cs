17886f546f461dfbb0c0e77d17c9e158750d5cb9
IEUser <IEUser>
2016-11-25 02:13:18 -0800

    Parallel designed Category and remove data clump e code dup

---
+++ src/TriviaRefactoringKata/Category.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Trivia
+{
+    public class Category
+    {
+        private readonly LinkedList<String> questions;
+        private String name;
+        private readonly int[] places;
+
+        public Category(string name, int[] places)
+        {
+            this.name = name;
+            this.places = places;
+            this.questions = new LinkedList<String>();
+        }
+
+        public String Name { get; private set; }
+
+        public void AddQuestion(String question)
+        {
+            this.questions.AddLast(question);
+        }
+
+        public bool Contains(int playerPlace)
+        {
+            return places.Contains(playerPlace);
+        }
+
+        public String NextQuestion()
+        {
+            var result = questions.First();
+            questions.RemoveFirst();
+            return result;
+        }
+    }
+}
--- src/TriviaRefactoringKata/QuestionDeck.cs
+++ src/TriviaRefactoringKata/QuestionDeck.cs
@@ -1,33 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 
 namespace Trivia
 {
     public class QuestionDeck
     {
-        private readonly LinkedList<string> popQuestions;
-        private readonly int[] popPlaces;
-
-        private readonly LinkedList<string> scienceQuestions;
-        private readonly int[] sciencePlaces;
-
-        private readonly LinkedList<string> sportsQuestions;
-        private readonly int[] sportPlaces;
-
-        private readonly LinkedList<string> rockQuestions;
-        private readonly int[] rockPlaces;
+        private readonly Category pop;
+        private Category science;
+        private Category sports;
+        private Category rock;
 
         public QuestionDeck()
         {
-            popQuestions = new LinkedList<string>();
-            scienceQuestions = new LinkedList<string>();
-            sportsQuestions = new LinkedList<string>();
-            rockQuestions = new LinkedList<string>();
-            popPlaces = new[] {0, 4, 6};
-            sciencePlaces = new[] { 1, 5, 9 };
-            sportPlaces = new[] { 2, 6, 10 };
-            rockPlaces = new[] { 3, 7, 11 };
+            pop = new Category("Pop", new[] { 0, 4, 6 });
+            science = new Category("Science", new[] { 1, 5, 9 });
+            sports = new Category("Sports", new[] { 2, 6, 10 });
+            rock = new Category("Rock", new[] { 3, 7, 11 });
         }
 
         String CreateQuestion(String category, int index)
@@ -39,41 +29,31 @@ namespace Trivia
         {
             for (int i = 0; i < 50; i++)
             {
-                popQuestions.AddLast(CreateQuestion("Pop", i));
-                scienceQuestions.AddLast(CreateQuestion("Science", i));
-                sportsQuestions.AddLast(CreateQuestion("Sports", i));
-                rockQuestions.AddLast(CreateQuestion("Rock", i));
+                pop.AddQuestion(CreateQuestion(pop.Name, i));
+                science.AddQuestion(CreateQuestion(science.Name, i));
+                sports.AddQuestion(CreateQuestion(sports.Name, i));
+                rock.AddQuestion(CreateQuestion(rock.Name, i));
             }
         }
 
         public String CategoryForPlace(int playerPlace)
         {
-            if (popPlaces.Contains(playerPlace)) return "Pop";
-            if (sciencePlaces.Contains(playerPlace)) return "Science";
-            if (sportPlaces.Contains(playerPlace)) return "Sports";
-            if (rockPlaces.Contains(playerPlace)) return "Rock";
+            if (pop.Contains(playerPlace)) return pop.Name;
+            if (science.Contains(playerPlace)) return science.Name;
+            if (sports.Contains(playerPlace)) return sports.Name;
+            if (rock.Contains(playerPlace)) return rock.Name;
 
             throw new InvalidOperationException("out of board");
         }
 
         public String AskQuestionCategory(String category)
         {
-            LinkedList<String> categoryQuestions = null;
-            if (category == "Pop") categoryQuestions = popQuestions;
-            if (category == "Science") categoryQuestions = scienceQuestions;
-            if (category == "Sports") categoryQuestions = sportsQuestions;
-            if (category == "Rock") categoryQuestions = rockQuestions;
-            if (categoryQuestions == null)
-                throw new InvalidOperationException($"Missing category {category}");
-            
-            return NextQuestion(categoryQuestions);
-        }
+            if (pop.Name == category) return pop.NextQuestion();
+            if (science.Name == category) return science.NextQuestion();
+            if (sports.Name == category) return sports.NextQuestion();
+            if (rock.Name == category) return rock.NextQuestion();
 
-        private string NextQuestion(LinkedList<string> questions)
-        {
-            var result = questions.First();
-            questions.RemoveFirst();
-            return result;
+            throw new InvalidOperationException($"Missing category {category}");
         }
     }
 }
